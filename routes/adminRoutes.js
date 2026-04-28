2c03239684bd4e56e16f974e6265b9af8a6a8f24
thoyyibtvt <mtpt1.muhammed@gmail.com>
2026-04-28 09:57:37 +0530

    works

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -804,7 +804,8 @@ router.put('/:id', verifyToken, async (req, res) => {
     }
 
     // Check permissions - only superadmin can update other admins, or admin can update themselves
-    if (req.admin.role !== 'superadmin' && req.admin._id.toString() !== req.params.id) {
+    if (req.admin.accessLevel !== 0 && req.admin._id.toString()
+      !== req.params.id) {
       return res.status(403).json({
         success: false,
         message: 'Not authorized to update this admin'
